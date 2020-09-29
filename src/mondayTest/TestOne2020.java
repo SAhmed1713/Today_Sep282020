3aeda542a8540050d79c20eede77ff0aef65d5b4
Tarishah <Tarishah@FourStars>
2020-09-28 21:47:39 -0400

    My First commit

---
+++ src/mondayTest/TestOne2020.java
@@ -0,0 +1,16 @@
+package mondayTest;
+
+public class TestOne2020 {
+	
+	public static void main (String args[]) {
+		
+		int x, y , answer;
+		x=20;
+		y=30;
+		answer=x+y;
+		
+		System.out.println("answer="+answer);
+		
+	}
+
+}
